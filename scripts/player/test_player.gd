cdf677e9ccf510e5174b1a38282567920d2cb249
Madalena Ye <mads1040@gmail.com>
2025-06-15 21:29:50 +0100

    camera

--- scripts/player/test_player.gd
+++ scripts/player/test_player.gd
@@ -13,13 +13,20 @@ var t_bob = 0.0
 const BASE_FOV = 75.0
 const FOV_CHANGE = 1.5
 
-@onready var head = $Head
-@onready var camera = $Head/Camera3D
+
+@onready var camera: Camera3D = %Camera3D
+
 @onready var health_label: Label = $HealthLabel
 @onready var health_component: Node = $HealthComponent
 
 var syncPos = Vector3.ZERO
 var is_paused := false
+
+@export_group("Camera")
+@export_range(0.0, 1.0) var mouse_sensitivity := 0.15
+
+var _camera_input_direction := Vector2.ZERO
+@onready var camera_pivot: Node3D = %CameraPivot
 
 
 func _ready() -> void:
@@ -47,10 +54,14 @@ func _unhandled_input(event: InputEvent) -> void:
 		return
 
 	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
-		if event is InputEventMouseMotion:
-			head.rotate_y(-event.relative.x * SENSITIVITY)
-			camera.rotate_x(-event.relative.y * SENSITIVITY)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		var is_camera_motion := (
+			event is InputEventMouseMotion and 
+			Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+		)
+		if is_camera_motion:
+			_camera_input_direction = event.screen_relative * mouse_sensitivity
+			
+			
 
 
 func _physics_process(delta: float) -> void:
@@ -58,50 +69,55 @@ func _physics_process(delta: float) -> void:
 		return
 
 	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
-		if not is_on_floor():
-			velocity += get_gravity() * delta
+		camera_pivot.rotation.x += _camera_input_direction.y * delta
+		camera_pivot.rotation.x = clamp(camera.rotation.x, -PI/6.0, PI/3.0)
+		camera_pivot.rotation.y -= _camera_input_direction.x * delta
+		
+		_camera_input_direction = Vector2.ZERO
+		#if not is_on_floor():
+			#velocity += get_gravity() * delta
+#
+		#if Input.is_action_just_pressed("jump") and is_on_floor():
+			#velocity.y = JUMP_VELOCITY
+#
+		#if Input.is_action_pressed("sprint") and is_on_floor():
+			#speed = SPRINT_SPEED
+		#else:
+			#speed = WALK_SPEED
+			#
+		#var input_dir := Input.get_vector("left", "right", "up", "down")
+		##var direction : Vector3 = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+#
+		##if is_on_floor():
+			##if direction:
+				##velocity.x = direction.x * speed
+				##velocity.z = direction.z * speed
+			##else:
+				##velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+				##velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+		##else:
+			##velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+			##velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+#
+		#syncPos = global_position
+#
+		#t_bob += delta * velocity.length() * float(is_on_floor())
+		#camera.transform.origin = _headbob(t_bob)
+#
+		#var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+		#var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+		#camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+#
+		#move_and_slide()
+	#else:
+		#global_position = global_position.lerp(syncPos, 0.5)
 
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
 
-		if Input.is_action_pressed("sprint") and is_on_floor():
-			speed = SPRINT_SPEED
-		else:
-			speed = WALK_SPEED
-			
-		var input_dir := Input.get_vector("left", "right", "up", "down")
-		var direction : Vector3 = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
-		if is_on_floor():
-			if direction:
-				velocity.x = direction.x * speed
-				velocity.z = direction.z * speed
-			else:
-				velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
-				velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
-		else:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
-
-		syncPos = global_position
-
-		t_bob += delta * velocity.length() * float(is_on_floor())
-		camera.transform.origin = _headbob(t_bob)
-
-		var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
-		var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
-		camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
-
-		move_and_slide()
-	else:
-		global_position = global_position.lerp(syncPos, 0.5)
-
-
-func _headbob(time) -> Vector3:
-	var pos = Vector3.ZERO
-	pos.y = sin(time * BOB_FREQ) * BOB_AMP
-	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
-	return pos
-
-func on_death() -> void:
-	get_tree().quit()
+#func _headbob(time) -> Vector3:
+	#var pos = Vector3.ZERO
+	#pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	#pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	#return pos
+#
+#func on_death() -> void:
+	#get_tree().quit()
