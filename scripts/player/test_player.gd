a24beb5fa17549c1fd5776d0e1807d8d62ba043c
Madalena Ye <mads1040@gmail.com>
2025-06-15 21:45:07 +0100

    fix rotations

--- scripts/player/test_player.gd
+++ scripts/player/test_player.gd
@@ -5,7 +5,7 @@ const WALK_SPEED = 5.0
 const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.003
-
+const ACCELERATION = 15.0
 const BOB_FREQ = 2.0
 const BOB_AMP = 0.08
 var t_bob = 0.0
@@ -19,6 +19,9 @@ const FOV_CHANGE = 1.5
 @onready var health_label: Label = $HealthLabel
 @onready var health_component: Node = $HealthComponent
 
+@onready var character: Node3D = %Patrick
+
+
 var syncPos = Vector3.ZERO
 var is_paused := false
 
@@ -26,8 +29,8 @@ var is_paused := false
 @export_range(0.0, 1.0) var mouse_sensitivity := 0.15
 
 var _camera_input_direction := Vector2.ZERO
+var _last_movement_direction := Vector3.BACK
 @onready var camera_pivot: Node3D = %CameraPivot
-
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -74,6 +77,26 @@ func _physics_process(delta: float) -> void:
 		camera_pivot.rotation.y -= _camera_input_direction.x * delta
 		
 		_camera_input_direction = Vector2.ZERO
+		
+		var raw_input := Input.get_vector("left", "right", "up", "down")
+		var 	forward := camera.global_basis.z
+		var right := camera.global_basis.x
+
+		var move_direction := forward * raw_input.y + right * raw_input.x
+
+		move_direction.y = 0.0
+		move_direction = move_direction.normalized()
+		
+		velocity = velocity.move_toward(move_direction * WALK_SPEED, ACCELERATION * delta)
+		
+		move_and_slide()
+		
+		if move_direction.length() > 0.2:
+			_last_movement_direction = move_direction
+		
+		var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
+		character.global_rotation.y = target_angle
+		
 		#if not is_on_floor():
 			#velocity += get_gravity() * delta
 #
